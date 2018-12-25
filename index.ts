8d091d250f8c5575d184047ae51c0889ae841d86
ergotamin <ergotamin.source@gmail.com>
2018-12-25 23:25:47 +0100

    [12/25/18] save [e439a177]

--- index.ts
+++ index.ts
@@ -1,4 +1,4 @@
-
-	[93m[1mAdded(B[m: [1m[32m0[90m changes ...(B[m
-
-	[38;5;20m[1mCommit(B[m:  '[90m[3m[12/22/18] index.js - ... [5d13c501](B[m'
+// import nbind-module
+import * as nbind from "nbind";
+// export the nbind.node-library
+export const lib = nbind.init<typeof nbind.Binding>(__dirname).lib;
